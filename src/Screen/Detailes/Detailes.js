c7298c51dffc917402ea0a23be57323439b31be1
Solanki Mayur <mayur.s@optimumfintech.co.in>
2023-07-21 16:58:40 +0530

    configuration

---
+++ src/Screen/Detailes/Detailes.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Detailes = () => {
+  return (
+    <View>
+      <Text>Detailes</Text>
+    </View>
+  )
+}
+
+export default Detailes
